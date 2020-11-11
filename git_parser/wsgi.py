df1f377b683ec5ec00e9a70e2e42a34e1e280c6d
stillnurs <noorsultan.mamataliev@gmail.com>
2020-11-11 21:52:39 +0600

    added environment variable settings

--- git_parser/wsgi.py
+++ git_parser/wsgi.py
@@ -11,6 +11,11 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+from dotenv import load_dotenv
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'git_parser.settings')
+
+project_folder = os.path.expanduser('~/Desktop/git_parser/git_parser/git_parser')
+load_dotenv(os.path.join(project_folder, '.env'))
 
 application = get_wsgi_application()
